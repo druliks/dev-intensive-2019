a749de0057c3d4a74a9b6bf9737a0b1b366bc85c
druliks <drulikbest@gmail.com>
2019-07-02 13:23:15 +0700

    hometask_2 complete try 3

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.devintensive.utils
 
 import java.lang.StringBuilder
 
+
+
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?,String?>{
         val parts:List<String>?=fullName?.split(" ")
@@ -19,277 +21,98 @@ object Utils {
     }
 
     fun transliteration(payload:String?, devider: String=" "): String {
-        val abcCyr = charArrayOf(
-            'а',
-            'б',
-            'в',
-            'г',
-            'д',
-            'е',
-            'ё',
-            'ж',
-            'з',
-            'и',
-            'й',
-            'к',
-            'л',
-            'м',
-            'н',
-            'о',
-            'п',
-            'р',
-            'с',
-            'т',
-            'у',
-            'ф',
-            'х',
-            'ц',
-            'ч',
-            'ш',
-            'щ',
-            'ъ',
-            'ы',
-            'ь',
-            'э',
-            'ю',
-            'я',
-            'А',
-            'Б',
-            'В',
-            'Г',
-            'Д',
-            'Е',
-            'Ё',
-            'Ж',
-            'З',
-            'И',
-            'Й',
-            'К',
-            'Л',
-            'М',
-            'Н',
-            'О',
-            'П',
-            'Р',
-            'С',
-            'Т',
-            'У',
-            'Ф',
-            'Х',
-            'Ц',
-            'Ч',
-            'Ш',
-            'Щ',
-            'Ъ',
-            'Ы',
-            'Ь',
-            'Э',
-            'Ю',
-            'Я',
-            'a',
-            'b',
-            'c',
-            'd',
-            'e',
-            'f',
-            'g',
-            'h',
-            'i',
-            'j',
-            'k',
-            'l',
-            'm',
-            'n',
-            'o',
-            'p',
-            'q',
-            'r',
-            's',
-            't',
-            'u',
-            'v',
-            'w',
-            'x',
-            'y',
-            'z',
-            'A',
-            'B',
-            'C',
-            'D',
-            'E',
-            'F',
-            'G',
-            'H',
-            'I',
-            'J',
-            'K',
-            'L',
-            'M',
-            'N',
-            'O',
-            'P',
-            'Q',
-            'R',
-            'S',
-            'T',
-            'U',
-            'V',
-            'W',
-            'X',
-            'Y',
-            'Z'
-        )
-        val abcLat = arrayOf(
-            "a",
-            "b",
-            "v",
-            "g",
-            "d",
-            "e",
-            "e",
-            "zh",
-            "z",
-            "i",
-            "i",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "r",
-            "s",
-            "t",
-            "u",
-            "f",
-            "h",
-            "c",
-            "ch",
-            "sh",
-            "sh",
-            "",
-            "i",
-            "",
-            "e",
-            "yu",
-            "ya",
-            "A",
-            "B",
-            "V",
-            "G",
-            "D",
-            "E",
-            "E",
-            "Zh",
-            "Z",
-            "I",
-            "I",
-            "K",
-            "L",
-            "M",
-            "N",
-            "O",
-            "P",
-            "R",
-            "S",
-            "T",
-            "U",
-            "F",
-            "H",
-            "C",
-            "Ch",
-            "Sh",
-            "Sh",
-            "",
-            "I",
-            "",
-            "E",
-            "Yu",
-            "Ya",
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z",
-            "A",
-            "B",
-            "C",
-            "D",
-            "E",
-            "F",
-            "G",
-            "H",
-            "I",
-            "J",
-            "K",
-            "L",
-            "M",
-            "N",
-            "O",
-            "P",
-            "Q",
-            "R",
-            "S",
-            "T",
-            "U",
-            "V",
-            "W",
-            "X",
-            "Y",
-            "Z"
-        )
 
-        var firstname=payload?.split(" ")?.getOrNull(0)
-        var lastname=payload?.split(" ")?.getOrNull(1)
-
-        val builder1 = StringBuilder()
-        if (firstname != null) {
-            for (i in 0 until firstname.length) {
-                for (x in abcCyr.indices) {
-                    if (firstname?.get(i)==abcCyr[x]) {
-                        builder1.append(abcLat[x])
-                    }
-                }
+        val parts=payload?.split(" ")
+        val result= StringBuilder("")
+        if (parts != null) {
+            for(part in parts){
+                result.append(toTranslitOld(part)).append(devider)
             }
         }
-        firstname=builder1.toString()
-
-        val builder2 = StringBuilder()
-        if (lastname != null) {
-            for (i in 0 until lastname.length) {
-                for (x in abcCyr.indices) {
-                    if (lastname?.get(i)==abcCyr[x]) {
-                        builder2.append(abcLat[x])
-                    }
-                }
-            }
-        }
-        lastname=builder2.toString()
-
-
-        return firstname+devider+lastname
+        result.deleteCharAt(result.lastIndex)
+        return result.toString()
     }
+
+    private fun toTranslitOld(text: String): String {
+        val mapLetters= mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya" ,
+            "А" to "A",
+            "Б" to "B",
+            "В" to "V",
+            "Г" to "G",
+            "Д" to "D",
+            "Е" to "E",
+            "Ё" to "E",
+            "Ж" to "Zh",
+            "З" to "Z",
+            "И" to "I",
+            "Й" to "I",
+            "К" to "K",
+            "Л" to "L",
+            "М" to "M",
+            "Н" to "N",
+            "О" to "O",
+            "П" to "P",
+            "Р" to "R",
+            "С" to "S",
+            "Т" to "T",
+            "У" to "U",
+            "Ф" to "F",
+            "Х" to "H",
+            "Ц" to "C",
+            "Ч" to "Ch",
+            "Ш" to "Sh",
+            "Щ" to "Sh'",
+            "Ъ" to "",
+            "Ы" to "I",
+            "Ь" to "",
+            "Э" to "E",
+            "Ю" to "Yu",
+            "Я" to "Ya")
+        val sb = StringBuffer(text.length)
+        for (i in 0 until text.length) {
+            val l = text.substring(i, i + 1)
+            if (mapLetters.containsKey(l)) {
+                sb.append(mapLetters[l])
+            } else {
+                sb.append(l)
+            }
+        }
+        return sb.toString()
+    }
+
 
     fun toInitials(firstName: String?, lastName: String?): String? {
         if(firstName==null&&lastName==null) return null
